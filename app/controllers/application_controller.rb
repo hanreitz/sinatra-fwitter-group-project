d5fa65b65f33d8f1a3f0480eeff182c783eaaa00
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-03-03 15:02:40 -0500

    commit tree 25041a9bf02a75984edc71ef41c3f2fcbdc97747

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,41 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, SecureRandom.hex(64)
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    if session[:user_id]
+      redirect to '/tweets/index'
+    else
+      erb :signup
+    end
+  end
+
+  post '/signup' do
+    if !params.values.any? {|v| v.empty?}
+      @user = User.create(params)
+      session[:user_id] = @user.id
+      redirect to '/tweets/index'
+    else
+      redirect to '/signup'
+    end
+  end
+
+  get '/login' do
+    erb :login
+  end
+
+  post '/login' do
+    
+  end
+
+  get '/tweets/index' do
   end
 
 end
